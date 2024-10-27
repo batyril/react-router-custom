6e07f28263f44d58232db5b59f006252fa3c21ac
ilnaz <237x237@gmail.com>
2024-10-27 20:10:00 +0300

    to make routing based on hash

---
+++ src/RouterHash.jsx
@@ -0,0 +1,46 @@
+import {useEffect, useState} from 'react'
+
+const HomePage = () => <p>Home Page</p>
+const AboutPage = () => <p>About Page</p>
+const UserPage = () => <p>User Page</p>
+
+const PAGES = {
+    HOME:'home',
+    ABOUT:'about',
+    USER:'user',
+}
+
+const getHashPage = () => location.hash.slice(1);
+
+function RouterHash() {
+  const [page, setPage] = useState(getHashPage())
+
+    const handleHashChange = () => {
+        const newPageHash = getHashPage()
+        setPage(newPageHash)
+    };
+
+    useEffect(() => {
+        window.addEventListener('hashchange',handleHashChange)
+        return () => {
+            window.removeEventListener('hashchange', handleHashChange);
+        };
+    },[])
+
+  return (
+    <>
+      <header>
+          <a href="#home">Home</a>
+          <a href="#about">About</a>
+          <a href="#user">User</a>
+      </header>
+        <main>
+            {page === PAGES.HOME && <HomePage/>}
+            {page === PAGES.ABOUT && <AboutPage/>}
+            {page === PAGES.USER && <UserPage/>}
+        </main>
+    </>
+  )
+}
+
+export default RouterHash
